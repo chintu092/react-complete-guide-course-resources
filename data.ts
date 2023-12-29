5e4f89fa45d76ef3f536152f70aab6e93bd54023
Supriyo Santra <57000917+chintu092@users.noreply.github.com>
2023-12-29 19:14:32 +0530

    Create data.ts

---
+++ data.ts
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+const Data = [
+  {
+    id: 1,
+    title: "First Blog Title",
+    category: "education",
+    desc:
+      "The Parthenon is a former temple on the Athenian Acropolis, regarded as an enduring symbol of Ancient Greece, democracy and Western civilization, and one of the world's greatest cultural monuments.",
+    thumbnail: "https://source.unsplash.com/ilQmlVIMN4c",
+    authorID: 3,
+  },
+  {
+    id: 2,
+    title: "Second Blog Title",
+    category: "science",
+    desc:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      thumbnail: "https://source.unsplash.com/WLxQvbMyfas",
+    authorID: 4,
+  },
+  {
+    id: 3,
+    title: "Third Blog Title",
+    category: "weather",
+    desc:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. It is often regarded as one of the 20th century's most famous and distinctive buildings",
+      thumbnail: "https://source.unsplash.com/JmuyB_LibRo",
+    authorID: 1,
+  },
+  {
+    id: 4,
+    title: "Fourth Blog Title",
+    category: "investment",
+    desc:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality. The fjord is one of Norway's most visited tourist sites.",
+      thumbnail: "https://source.unsplash.com/3PeSjpLVtLg",
+    authorID: 2,
+  },
+];
+
+export default Data;
